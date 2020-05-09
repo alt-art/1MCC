94ca1cf3d04a1c18a5a547e4cacc355e05fa516b
alt-art <pedromendescraft@gmail.com>
2020-05-09 16:45:10 -0300

    walktenminute

---
+++ walktenminute/main.js
@@ -0,0 +1,21 @@
+let input = ['n','w','s','e','n','w','s','e','n','s']
+
+let y = 0, x = 0;
+
+input.forEach(point=>{
+  switch (point) {
+    case 'n':
+      y++;
+      break;
+    case 's':
+      y--;
+      break;
+    case 'w':
+      x++;
+      break;
+    case 'e':
+      x--;
+      break;
+  }
+})
+console.log(y === 0 && x === 0 && input.length === 10)
